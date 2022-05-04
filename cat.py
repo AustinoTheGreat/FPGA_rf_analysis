a35128c3a18daf871ebb8f1d46a460c0baa757a0
AustinoTheGreat <53872659+AustinoTheGreat@users.noreply.github.com>
2022-05-04 16:39:02 -0400

    improvements on envelope signal & comments

--- cat.py
+++ cat.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" 18 April      v2.1 "Prowler"
-@author: yujun, austin
-This file computes the frequency-domain signal energy plot over time
-for each bitsteam upload intercept set of csv files. We hope to see
-certain patterns between different sets of data. 
+""" May 4th
+@author: Austin Zhu
+Read .bit and rf files
+3 Main goals
+A: Peaks matching/comparison between bitstream transitions and rf energy data
+B: Re-engineer JTAG freqeuncy from rf data
+C: Envelop matching/comparison between chosen bitstream transition and rf energy data
 
-To use this file, copy all the relevant CSV files to the Computations folder
-and run. The energies computed are exported to a new CSV. """
-# path name: cd /Users/yujun/Desktop/EENG428_FPGA/Computations
+"""
 
 import csv
 from fileinput import filename
@@ -26,19 +26,25 @@ from scipy.signal import find_peaks
 from numpy import array, sign, zeros
 from scipy.interpolate import interp1d
 from matplotlib.pyplot import plot,show,grid
-
 import os
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
+"""*************** 0. Data Structure definitions ***************"""
 class bitstream:
+    """
+    Stores bistream bits and data
+    """
     def __init__(self, filename, bins):
         self.bits, self.zeros, self.ones, self.bins, self.test_flips = process_bistream(filename, bins)
         self.peaks, self.bound = get_peaks(self.test_flips)
         
 
 class allData:
+    """
+    Stores rf data and its analytics
+    """
     def __init__(self, data, energy, ifft):
         self.data = data
         self.energy = energy
@@ -47,6 +53,9 @@ class allData:
         self.peaks, self.bound = get_peaks(self.energy)
 
 class rfData:
+    """
+    Stores individual freqeuncy spectrum data directly read from CSV
+    """
     def __init__(self, freq, mag):
         self.freq = freq
         self.mag = mag
@@ -54,26 +63,13 @@ class rfData:
 
 """*************** 1. Function definitions ***************"""
 
-
 def read_values(file_name):
     """
     Reads in the frqeuency spectrum information of a csv with file_name
     Returns two lists - frequency, magnitude
     Assumes data starts at row 22
-    Subtracts from noise_file
     """
-    # # get noise
-    # file_n = open(noise_file)
-    # data_n = file_n.readlines()
-    # noise_mag = []
-    # noise = data_n[-1].split(',')
-    # for row_count_n in range(len(data_n)):
-    #     if row_count_n >= 22: 
-    #         f_n = data_n[row_count_n].split(',')
-    #         if len(f_n) == 2:   # ignore /n row
-    #             noise_mag.append(10**(float(f_n[1])/20))
-    #             #print("Noise:", round(10**(float(f_n[1])/20), 4))
-    # file_n.close()
+
     file = open(file_name)
     data = file.readlines()
 
@@ -85,47 +81,48 @@ def read_values(file_name):
             if len(f) == 2:   # ignore /n row
                 freq.append(float(f[0]))
                 mag.append(10**(float(f[1])/20))
-                #if float(f[1]) > -63:                
-                #    mag.append(10**(float(f[1])/20))
-                #else:
-                #    mag.append(0)
+
     file.close()
-    
-    # for idx in range(len(mag)):
-    #     #print("Before:",mag[idx], " after: ",mag[idx] - noise_mag[idx])
-    #     mag[idx] = mag[idx] - noise_mag[idx]    # subtract linearized version
     
     return freq, mag
 
 def read_binary_file(fileName):
     """
-    Read binary bitstream files
+    Reads in binary bitstream files
+    Returns input file as a list of bytes
     """
     with open(fileName, mode='rb') as file: # b is important -> binary
         fileContent = file.read()
     return fileContent
 
 def inverse_transform_helper(data):
+    """
+    Reads in a specific instance of data from rf at JTAG during loading
+    Returns its corresponding time-domain data
+    """
     ifft = []
     for i, item in enumerate(data):
         ifft.append(inverse_transform(item.mag))
-
 
     return ifft
         
 
 def inverse_transform(freq_domain):
     """
+    Reads in list of freqeuncy domain data
     Calculates the inverse real fast fourier transform from the Numpy library
     on the given freqeuncy domain data
+    Returns time-domain data
     """
+
     a = np.fft.irfft(freq_domain)
     return a
 
 def compute_energy(freq, mag):
     """
+    Reads in a specific spectrum snapshot
     Calculates the signal energy, integrating amplitude^2 over frequency
-    Input is spectrum dictionary and outputs the energy quantity
+    Returns the energy quantity
     """
     num_samples = len(freq)   # same as length of magnitude list
     energy = 0
@@ -138,25 +135,32 @@ def compute_energy(freq, mag):
 
 
 def process_signal(option):
+    """
+    Reads in a folder name with rf data
+    Performs various analysis
+    Returns data structure storing all data and analytics
+    """
+
     dir_list = './' + option + '/'
 
     # Get list of all files in a given directory sorted by name
     list_of_files = sorted(filter(os.path.isfile, glob.glob(dir_list + '*')))
 
-    # filenames = generate_file_name()
     list_of_data = []
     list_of_energies = []
     list_of_iffts = []
 
     for i, item in enumerate(list_of_files):
-        this_freq, this_mag = read_values(item)
-        this_energy = compute_energy(this_freq, this_mag)
+
+        this_freq, this_mag = read_values(item) # Reads in frequency and magnitude of given slice
+        this_energy = compute_energy(this_freq, this_mag) # Calculates energy
         
         if this_energy < 800:
+            # Filters out data when JTAG has no bitstream input action
             pass
         else:
             this_csv = rfData(this_freq, this_mag)
-            this_ifft = inverse_transform(this_mag)
+            this_ifft = inverse_transform(this_mag) # IRFFT
 
             list_of_data.append(this_csv)
             list_of_energies.append(this_energy)
@@ -164,12 +168,8 @@ def process_signal(option):
 
     
     result = allData(list_of_data, list_of_energies, list_of_iffts)
-
     print("RF files read for folder " + option)
-
     return result
-
-
 
 def generate_file_name(start, end, option):
     """
@@ -193,6 +193,7 @@ def generate_file_name(start, end, option):
 
 def flip_counter(full_list, sampling):
     """
+    Reads in bitstream data and sampling rate
     Function to down-sample full_list (high frequency signal of 0 and 1s)
     into chunks of width sampling, and counts the number of 0->1 or 1->0
     transitions within each chunk. 
@@ -215,6 +216,12 @@ def flip_counter(full_list, sampling):
     return total_flips
 
 def process_bistream(filename, bins):
+    """
+    Reads in filename of bitstream and number of bins for bitstream to be broken down into
+    Calculates number of bit transitions, 0->1 / 1->0
+    Returns bitstream read-in and its analytics
+    """
+
     bits = read_binary_file(filename)
 
     # compute number of 1s and 0s
@@ -227,11 +234,14 @@ def process_bistream(filename, bins):
 
     print("Reading bitstream file " + filename)
 
-    for x, i in enumerate(bits):           # iterate over each byte
+    for x, i in enumerate(bits): # Iterates over each byte
 
-        i_bin = bin(i)[2:]    # make binary string e.g. '0b110' gut the 0b part
+        # make binary string e.g. '0b110' gut the 0b part
+        i_bin = bin(i)[2:] 
+
+        # fit to e.g. 00000110 format
         if len(i_bin) != 8:
-            i_bin = '0' * (8 - len(i_bin)) + i_bin   # fit to e.g. 00000110 format
+            i_bin = '0' * (8 - len(i_bin)) + i_bin
 
         for idx in range(len(i_bin)):
             if i_bin[idx] == '1':
@@ -244,20 +254,21 @@ def process_bistream(filename, bins):
     # print(filename)
     # print("Number of bytes: " + str(len(bits)) + "\nNumber of zeros: " + str(zeros) + "\nNumber of ones: " + str(ones))
 
-    # binned plot of theoretical information signature using bins
-
-    # count how many transitions within chunks of this size
+    # Counts how many transitions within chunks of this size
     sampling = len(bitValues) // bins 
+
     # print("To achieve", bins, "bins, sample at", sampling, "size.")
     print("Counting 0/1 transitions in bitstream file " + filename)
-    test_flips = flip_counter(bitValues, sampling)
-
-    print("Bitstream imported")
-
-
+    test_flips = flip_counter(bitValues, sampling) # Counts number of flips
+    print("Bitstream imported for file " + filename)
     return bits, zeros, ones, bins, test_flips
 
 def remove_extra_signal(signal):
+    """
+    Reads in signal
+    Returns signal with non-active files removed
+    *** Deprecated for newest version of code
+    """
     energies = signal.energy
 
     for i, item in enumerate(energies):
@@ -268,34 +279,38 @@ def remove_extra_signal(signal):
         else:
             pass
     signal.ifft = inverse_transform_helper(signal.data)
-
     signal.peaks, signal.bound = get_peaks(signal.energy)
-    
     return signal
     
 def get_peaks(test_list):
-    
-
+    """
+    Reads in values of a signal in a list
+    Calculates mean, s.d., and find peaks based on highest x percentile
+    Return peaks as a list of indices
+    """
     mean = sum(test_list) / len(test_list)
     variance = sum([((x - mean) ** 2) for x in test_list]) / len(test_list)
     res = variance ** 0.5
 
-    bound = mean + 1.5 * res # two standard deviation above mean, 95% percentile of data treated as peaks 87%
+    bound = mean + 1.5 * res # 1.5 standard deviation above mean, 87% percentile of data treated as peaks
 
     peaks, _ = find_peaks(test_list, height = bound)
-    # print(peaks)
     return peaks, bound
 
 def print_peaks(all_data, all_bitstreams):
+    """
+    Reads in all data collected
+    Visualizes all rf energy and bitstream transistion datasets side-by-side, with peaks
+    """
+
+    # Creates a subplot
     fig, axes = plt.subplots(nrows=len(all_data), ncols=2, figsize=(12,8))
-    # print(axes.shape)
-    # print(len(all_data))
     i = 0
 
     if len(all_data) == 1:
-        axes[0].plot(range(0, len(all_data[i].energy)), all_data[i].energy)
-        res_list = [all_data[i].energy[j] for j in all_data[i].peaks]
-        axes[0].plot(all_data[i].peaks, res_list, "x")
+        axes[0].plot(range(0, len(all_data[i].energy)), all_data[i].energy) # Plot the data
+        res_list = [all_data[i].energy[j] for j in all_data[i].peaks] # Finds peaks of data
+        axes[0].plot(all_data[i].peaks, res_list, "x") # Plots the peaks
         axes[0].set_title('Energy Graph for RF Dataset Number ' + str(i))
         axes[0].set_xlabel('Sample Count', fontsize = 8)
 
@@ -306,7 +321,6 @@ def print_peaks(all_data, all_bitstreams):
         axes[1].set_xlabel('Sample Count', fontsize = 8)
 
     else:
-
         for i in range(0, len(all_data)):
             axes[i, 0].plot(range(0, len(all_data[i].energy)), all_data[i].energy)
             res_list = [all_data[i].energy[j] for j in all_data[i].peaks]
@@ -325,21 +339,20 @@ def print_peaks(all_data, all_bitstreams):
 
     return
 
-def get_envelop(all_data, all_bitstreams):
-
-    return
-
 def get_one_envelop(input):
+    """
+    Reads in values of a signal as a list
+    Referenced https://stackoverflow.com/questions/34235530/how-to-get-high-and-low-envelope-of-a-signal
+    Calculates an upper envelope of given signal
+    Returns signal and envelope signal
+    """
 
-    # Code referenced from https://stackoverflow.com/questions/34235530/how-to-get-high-and-low-envelope-of-a-signal
-
-    s = array(input) #This is your noisy vector of values.
+    s = array(input) # Vector of values
 
     q_u = zeros(s.shape)
     q_l = zeros(s.shape)
 
     #Prepend the first value of (s) to the interpolating values. This forces the model to use the same starting point for both the upper and lower envelope models.
-
     u_x = [0,]
     u_y = [s[0],]
 
@@ -347,7 +360,6 @@ def get_one_envelop(input):
     l_y = [s[0],]
 
     #Detect peaks and troughs and mark their location in u_x,u_y,l_x,l_y respectively.
-
     for k in range(1,len(s)-1):
         if (sign(s[k]-s[k-1])==1) and (sign(s[k]-s[k+1])==1):
             u_x.append(k)
@@ -358,7 +370,6 @@ def get_one_envelop(input):
             l_y.append(s[k])
 
     #Append the last value of (s) to the interpolating values. This forces the model to use the same ending point for both the upper and lower envelope models.
-
     u_x.append(len(s)-1)
     u_y.append(s[-1])
 
@@ -366,7 +377,6 @@ def get_one_envelop(input):
     l_y.append(s[-1])
 
     #Fit suitable models to the data. Here I am using cubic splines, similarly to the MATLAB example given in the question.
-
     u_p = interp1d(u_x,u_y, kind = 'cubic',bounds_error = False, fill_value=0.0)
     l_p = interp1d(l_x,l_y,kind = 'cubic',bounds_error = False, fill_value=0.0)
 
@@ -375,17 +385,23 @@ def get_one_envelop(input):
         q_u[k] = u_p(k)
         q_l[k] = l_p(k)
 
-    #Plot everything
     # plot(s);hold(True);plot(q_u,'r');plot(q_l,'g');grid(True);show()
-    # plot(s);plot(q_u,'r');plot(q_l,'g');grid(True);show()
-    
+    # plot(s);plot(q_u,'r');plot(q_l,'g');grid(True);show()    
     # plot(s, alpha = 0.3); plot(q_u,'r'); grid(True);show()
+
+    # Implements min and max caps to prevent non-converging tail endings
+    max_cap = max(s) * 1.2
+    for i in range(0, len(q_u)):
+        if q_u[i] < 0:
+            q_u[i] = 0
+        elif q_u[i] > max_cap:
+            q_u[i] = max_cap
 
     return s, q_u
 
     return
 
-# """************ 2. Working with csv files (oscilloscope) ************"""
+# """************ 2. Main Section of User Interaction ************"""
 
 all_data = []
 all_bitstreams = []
@@ -393,15 +409,18 @@ all_bitstreams = []
 input_num = input("Number of datasets to categorize: ")
 i = 0
 
+# Reads in rf and bitstream data
 for i in range (1, int(input_num) + 1):
     all_data.append(process_signal(str(i)))
     all_bitstreams.append(bitstream(str(i) + '.bit', all_data[i - 1].num_files))
 
+# Deprecated signal filtering code
 # for i in range(0, len(all_data)):
 #     all_data[i] = remove_extra_signal(all_data[i])
 
 print("Number of datasets: " + str(len(all_data)))
 
+# Main UI loop
 input_choice = ""
 while(input_choice != 'q'):
     input_choice = input("Import complete, choose between a(peaks), b(JTAG freq.), c(envelop), q(quit): ")
@@ -428,94 +447,14 @@ while(input_choice != 'q'):
         axes[0].set_title('Energy Graph for RF Dataset Number ' + str(rf_choice))
         axes[0].set_xlabel('Sample Count', fontsize = 8)
 
-        axes[1].plot(s2, alpha = 0.3); axes[0].plot(q_u2,'r'); axes[0].grid(True)
+        axes[1].plot(s2, alpha = 0.3); axes[1].plot(q_u2,'r'); axes[1].grid(True)
         axes[1].set_title('0/1 Transitions in Bitstream for Bitstream Number ' + str(bit_choice))
         axes[1].set_xlabel('Sample Count', fontsize = 8)
-
+        
+        fig.tight_layout()
         show()
 
     elif input_choice == 'q':
         print("Program ends")
     else:
         print("Invalid input")
-
-
-# """*************** 3. Working with bitsteam file ***************"""
-
-
-# # read binary file and show its values
-# binaryFileName = "ro_11percentLUTs.bit"
-# #ac701.bit   kc705.bit    vcu118.bit
-
-# x = read_binary_file(binaryFileName)
-# print(type(x))
-
-
-
-
-
-
-# test_counter, test_flips = flip_counter(bitValues, sampling)
-# print("test_flips:", test_flips)
-
-
-# """*************** 4. Plot and save analysis***************"""
-
-
-# # make a binned version, to match with our energy signature
-# fig_tile = "Binary File Transition Values"
-# ax1 = plt.subplot()
-# l1, = ax1.plot(normalize(energies), color='red', alpha = 0.5)
-# ax2 = ax1.twinx()
-# l2, = ax2.plot(test_flips, 'bo') # 'ro'
-# plt.legend([l1,l2],["Normalized","Bitstream"])
-# plt.xlabel("Time")
-# plt.ylabel("Transitions")
-# plt.title(fig_tile)
-# plt.savefig(fig_tile+".png")
-# plt.show()
-
-
-# fig_tile = "Time Domain Data From IRFFT"
-# plt.plot(range(0, len(timeDomain)), timeDomain)
-# plt.xlabel("Sample count")
-# plt.ylabel("Magnitude")
-# plt.title(fig_tile)
-# plt.savefig(fig_tile+".png")
-# plt.show()
-
-# fig_tile = "Energy signature (original)"
-# plt.plot(times, energies)
-# plt.xlabel("Sample count")
-# plt.ylabel("Frequency-domain signal energy")
-# plt.title(fig_tile)
-# plt.savefig(fig_tile+".png")
-# plt.show()
-
-# fig_tile = "Energy signature (normalized)"
-# plt.plot(times, normalize(energies))
-# plt.xlabel("Sample count")
-# plt.ylabel("Frequency-domain signal energy")
-# plt.title(fig_tile)
-# plt.savefig(fig_tile+".png")
-# plt.show()
-
-# fig_tile = "Energy signature (log)"
-# plt.plot(times, get_log(energies))
-# plt.xlabel("Sample count")
-# plt.ylabel("Frequency-domain signal energy")
-# plt.title(fig_tile)
-# plt.savefig(fig_tile+".png")
-# plt.show()
-
-# # open the file in the write mode
-# f_out = open('energy0.csv', 'w')
-# writer = csv.writer(f_out)
-# for value in energies:
-#     # write a row to the csv file
-#     writer.writerow([float(value)])
-# # close the file
-# f_out.close()
-
-# print("All computations done, energies exported")
-
